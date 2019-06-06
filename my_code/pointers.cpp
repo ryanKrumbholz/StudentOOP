f3e7c8ac5c8fc86ab91799055ff61be73361785b
Ryan Krumbholz <ryankrumbholz@10-21-46-224.dynapool.wireless.nyu.edu>
2019-06-06 13:37:29 -0400

    Pointers resubmit

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -1,15 +1,16 @@
+
 #include <iostream>
 #include "pointers.h"
 
 using namespace std;
 
 void inc_by_value(int n) {
-    n++;
+    ++n;
 }
 
 
 void inc_with_pointer(int* iptr) {
-    ++(*iptr);
+    (*iptr)++;
 }
 
 
@@ -19,49 +20,44 @@ void inc_with_reference(int& n) {
 
 
 Thing** create_array_of_things(int n) {
-    Thing** thingsarray = new Thing*[n]; //  this allocates memory and creates an array of things
-    for(int i=0; i<n;i++){
-        thingsarray[i] = new Thing(i);
+    Thing** thing = new Thing*[n];
+    for (int i = 0; i < n; i++) {
+        thing[i] = new Thing(i);
     }
-    return thingsarray;
+    return thing;
 }
 
 void print_all_things(Thing** things, int n) {
-    for(int i=0; i<n;i++){
-        cout << things[i] -> val <<" "; // you are printing everything out from the pointer
-                                        // inside of things array
+    for (int i = 0; i < n; i++) {
+        cout << things[i]->val << ' ';
     }
-    cout << endl; //end the line
 }
 
 void double_all_things(Thing** things, int n) {
-    for(int i = 0; i < n ; i++){
-        things[i] -> val *=2; //this doubles every value by accesing it, then it changes
-                             //the value by multiplting it by two; this can also be done by
-                            // *(things[i]).val = i
+    for (int i = 0; i < n; i++) {
+        things[i] -> val *= 2;
     }
 }
 
 void delete_all_things(Thing** things, int n) {
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         delete things[i];
     }
-    delete[] things;
+    delete [] things;
 }
 
 
 void assignTA(Student* s, Student* ta) {
-    s -> ta = ta; //takes the student and uses the TA to assign
+    s->ta= ta;
 }
 
-void printTAs(vector<Student*> student) {
-    for (Student* students : student) {
-        if (students -> ta == nullptr) {
-            cout << students -> name << " has no TA" << endl;
+void printTAs(vector<Student*> students) {
+    for (Student* student : students) {
+        if(student -> ta == nullptr) {
+            cout << student -> name << " has no ta\n";
         }
         else {
-            cout << students->name << " has TA " << students ->ta->name << endl;
+            cout << student -> name << " has ta " << student -> ta -> name << endl;
         }
     }
 }
-
