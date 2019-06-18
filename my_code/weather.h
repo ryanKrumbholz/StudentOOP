dc96eb3a63ca92149c54fcafa973906eed5b8712
Ryan Krumbholz <ryankrumbholz@10-18-242-145.dynapool.wireless.nyu.edu>
2019-06-18 13:19:09 -0400

    Vector fix

--- my_code/weather.h
+++ my_code/weather.h
@@ -4,11 +4,51 @@
 #include <string>
 #include <vector>
 
+/*
+ * This will be the base class for all types of images.
+ * */
+class Image {
+ public:
+    Image(int w, int h, std::string flnm);
+    // copy constructor:
+    Image(const Image& img2);
+    ~Image();
+    Image& operator=(const Image& img2);
+    int image_sz();
+
+    /*
+     * Setting `display() = 0` here makes this an abstract
+     * class that can't be implemented.
+     * */
+    std::string display(std::string s);
+    /*
+     * If we don't want virtual method lookup, we
+     * could just declare:
+     * void display();
+     * */
+
+    int get_height() { return height; }
+    int get_width() { return width; }
+
+ private:
+    int width;
+    int height;
+    std::string filename;
+    char* image_buf;
+    void copy_fields(const Image& img2);
+};
+
+class Gif : public Image {
+    public:
+        Gif(int w, int h, std::string flnm) : Image(w,h,flnm){}
+};
+
+
 struct GPS {
     double latitude;
     double longitude;
     GPS(double la, double lo) : latitude(la),
-    longitude(lo) {}
+        longitude(lo) {}
 };
 
 std::ostream& operator<<(std::ostream& os, const GPS& gps);
@@ -16,26 +56,27 @@ std::ostream& operator<<(std::ostream& os, const GPS& gps);
 
 class Date {
     friend std::ostream& operator<<(std::ostream& os, const Date& date);
-public:
-    Date(int d, int m, int y) ;
-private:
-    int day;
-    int month;
-    int year;
+ public:
+    Date(int d, int m, int y);
+ private:
+     int day;
+     int month;
+     int year;
 };
 
 
 class WReading {
     friend std::ostream& operator<<(std::ostream& os, const WReading& wr);
-public:
+ public:
     WReading(Date dt, double temp, double hum, double ws) :
-    date(dt), temperature(temp), humidity(hum), windspeed(ws)
-    {}
+        date(dt), temperature(temp), humidity(hum), windspeed(ws)
+    {
+    }
+
     double get_tempF();
-    double get_tempC() {return temperature;}
-    double get_heat_index();
-    double get_wind_chill();
-private:
+    double get_tempC() { return temperature; }
+
+ private:
     Date date;
     double temperature;  // stored temp in C
     double humidity;
@@ -51,13 +92,13 @@ const int GOOD = 2;
 class Weather {
     // << should output name, rating, and use the GPS << to output my_loc
     friend std::ostream& operator<<(std::ostream& os, const Weather& w);
-public:
+ public:
     Weather(std::string nm, GPS loc);
     std::string get_name() const;
     int get_rating() const;
     void set_rating(int new_rating);
     void add_reading(WReading wr);
-private:
+ private:
     std::vector<WReading> wreadings;
     std::string station_nm;
     GPS my_loc;
